4fecf2b6ed0818da9bec507aec766a5edcbd8467
pamudithasandaru <pamudithasandaru2002@gmail.com>
2025-08-03 20:00:15 +0530

    Setting User authentication using clerk
    edited by adding mybookings button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react'
 import { assets } from '../assets/assets'
-import { MenuIcon, SearchIcon, XIcon } from 'lucide-react'
-import { Link } from 'react-router-dom'
+import { MenuIcon, SearchIcon, TicketPlus, XIcon } from 'lucide-react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 
 const Navbar = () => {
 
   const [isOpen, setIsOpen] = useState(false)
+  const {user} = useUser()
+  const {openSignIn} = useClerk()
+
+  const navigate = useNavigate()
 
   return (
     <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5'>
@@ -28,7 +33,19 @@ const Navbar = () => {
 
         <div className='flex items-center gap-8'>
             <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer' />
-            <button className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>Login</button>
+            {
+                !user ? (
+                    <button onClick={openSignIn} className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>Login</button>
+                ) : (
+                    <UserButton>
+                        <UserButton.MenuItems>
+                            <UserButton.Action label="My Bookings" labelIcon={<TicketPlus width={15}/>}
+                            onClick={()=> navigate('/my-bookings')}/>
+                        </UserButton.MenuItems>
+                    </UserButton>
+                )
+            }
+            
         </div>
         {/* this menuicon will be displayed only when screen is small */}
         <MenuIcon className='max-md:ml-4 md:hidden w-8 h-8 cursor-pointer' 
