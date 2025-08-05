dad4656996ec6f2f56ec5a6772958b35f3a695c2
pamudithasandaru <pamudithasandaru2002@gmail.com>
2025-08-06 01:43:31 +0530

    MovieDetails Part 3

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -46,15 +46,27 @@ const MovieDetails = () => {
             <button className='flex items-center gap-2 px-7 py-3 text-sm bg-gray-800 hover:bg-gray-900 transition rounded-md font-medium cursor-pointer active:scale-95'>
               <PlayCircleIcon className="w-5 h-5"/>Watch Trailer
               </button>
-            <a href="" className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer active:scale-95'>
+            <a href="#dateSelect" className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer active:scale-95'>
               Buy Tickets</a>
             <button className='bg-gray-700 p-2.5 rounded-full transitioncursor-pointer active:scale-95'>
               <Heart className={`w-5 h-5`} />
             </button>
           </div>
-
         </div>
       </div>
+
+      <p className='text-lg font-medium mt-20'>Your Favorite Cast</p>
+      <div className='overflow-x-auto no-scrollbar mt-8 pb-4'>
+        <div className='flex items-center gap-4 w-max px-4'>
+        {show.movie.casts.slice(0,12).map((cast,index)=>(
+          <div key={index} className='flex flex-col items-center text-center'>
+            <img src={cast.profile_path} alt="" className='rounded-full h-20 md:h-20 aspect-square object-cover' />
+            <p className='font-medium text-xs mt-3'>{cast.name}</p>
+          </div>
+        ))}
+        </div>
+      </div>
+
     </div>
   ) : <div>Loading...</div>
 }
