5bc44a9fd16db246158776ab4056801005e2cea1
pamudithasandaru <pamudithasandaru2002@gmail.com>
2025-08-06 00:33:39 +0530

    MovieDetails Part 1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,13 @@ import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import Movies from './pages/Movies'
-import MovieDetails from './pages/MovieDetails'
+// import MovieDetails from './pages/MovieDetails'
 import SeatLayout from './pages/SeatLayout'
 import MyBookings from './pages/MyBookings'
 import Favorite from './pages/Favorite'
 import {Toaster} from 'react-hot-toast'
 import Footer from './components/Footer'
+import MovieDetails from './pages/MovieDetails'
 
 const App = () => {
   const isAdminRoute = useLocation().pathname.startsWith('/admin')
@@ -20,8 +21,8 @@ const App = () => {
      <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/movies' element={<Movies/>} />
-      <Route path='/movies:id' element={<MovieDetails/>} />
-      <Route path='/movies:id/:date' element={<SeatLayout/>} />
+      <Route path='/movies/:id' element={<MovieDetails/>} />
+      <Route path='/movies/:id/:date' element={<SeatLayout/>} />
       <Route path='/my-bookings' element={<MyBookings/>} />
       <Route path='/favorite' element={<Favorite/>} />
      </Routes>
--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -6,7 +6,7 @@ import timeFormat from '../lib/timeFormat';
 const MovieCard = ({movie}) => {
     const navigate = useNavigate();
   return (
-    <div className='flex flex-col jusstify-between p-3 bg-gray-800 rounded-2x1 hover:-translate-y-1 transition duration-300 w-66'>
+    <div className='flex flex-col justify-between p-3 bg-gray-800 rounded-2x1 hover:-translate-y-1 transition duration-300 w-66'>
         <img onClick={()=> {navigate(`/movies/${movie._id}`); 
         scrollTo(0, 0)}} src={movie.backdrop_path} alt="" className='rounded-lg h-52 w-full object-cover object-right-bottom cursor-pointer'/>
         <p className='font-semibold mt-2 truncate'>{movie.title}</p>   
