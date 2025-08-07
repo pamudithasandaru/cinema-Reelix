3f95891b2c89345849fff2fdc6332186d441d9c1
pamudithasandaru <pamudithasandaru2002@gmail.com>
2025-08-07 12:40:15 +0530

    Admin Pages Initiated
    Admin Navbar design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,11 @@ import Favorite from './pages/Favorite'
 import {Toaster} from 'react-hot-toast'
 import Footer from './components/Footer'
 import MovieDetails from './pages/MovieDetails'
+import Layout from './pages/admin/Layout'
+import Dashboard from './pages/admin/Dashboard'
+import AddShows from './pages/admin/AddShows'
+import ListShows from './pages/admin/ListShows'
+import ListBookings from './pages/admin/ListBookings'
 
 const App = () => {
   const isAdminRoute = useLocation().pathname.startsWith('/admin')
@@ -25,6 +30,12 @@ const App = () => {
       <Route path='/movies/:id/:date' element={<SeatLayout/>} />
       <Route path='/my-bookings' element={<MyBookings/>} />
       <Route path='/favorite' element={<Favorite/>} />
+      <Route path='/admin/*' element={<Layout/>}>
+        <Route index element={<Dashboard />}/>
+        <Route path="add-shows" element={<AddShows />} />
+        <Route path="list-shows" element={<ListShows />} />
+        <Route path="list-bookings" element={<ListBookings />} />
+      </Route>
      </Routes>
      {!isAdminRoute && <Footer/>}
     </>
