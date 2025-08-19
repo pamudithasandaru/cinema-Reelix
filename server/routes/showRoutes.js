bbe6611e56cd12adf906d6536c77decc7bd5f9e7
pamudithasandaru <pamudithasandaru2002@gmail.com>
2025-08-19 20:14:06 +0530

    middleware development

--- server/routes/showRoutes.js
+++ server/routes/showRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getNowPlayingMovies } from "../controllers/showController.js";
+import { addShow, getNowPlayingMovies } from "../controllers/showController.js";
+import { protectAdmin } from "../middleware/auth.js";
 
 const showRouter = express.Router();
 
-showRouter.get('/now-playing', getNowPlayingMovies)
+showRouter.get('/now-playing',protectAdmin, getNowPlayingMovies)
+showRouter.post('/add',protectAdmin, addShow)
 
 export default showRouter;
