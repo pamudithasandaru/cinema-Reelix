0d190b456e006faf97edde0fe5edeb9fac1bbeaa
pamudithasandaru <pamudithasandaru2002@gmail.com>
2025-08-20 10:49:24 +0530

    booking model

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -82,14 +82,14 @@ export const addShow = async (req, res) =>{
 }
 
 //API to get all shows from the database
-export const getShows = async(req, res) => {
+export const getShows = async(req, res) =>{
     try {
         const shows = await Show.find({showDateTime: {$gte: new Date()}}).populate('movie').sort({ showDateTime: 1 });
 
         //filter unique shows
         const uniqueShows = new Set(shows.map(show => show.movie))
 
-        res.json({success: true, shows: Array.from(uniqueShows)})
+        res.json({ success: true, shows: Array.from(uniqueShows)})
     } catch (error) {
         console.error(error);
         res.json({ success: false, message: error.message});
