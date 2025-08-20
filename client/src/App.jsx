f8ed2c85a1047c5198d49354da67aef775231e85
pamudithasandaru <pamudithasandaru2002@gmail.com>
2025-08-20 17:39:55 +0530

    api linking in process
    this part may have troubles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,9 +15,14 @@ import Dashboard from './pages/admin/Dashboard'
 import AddShows from './pages/admin/AddShows'
 import ListShows from './pages/admin/ListShows'
 import ListBookings from './pages/admin/ListBookings'
+import { SignIn } from '@clerk/clerk-react'
+import { useAppContext } from './context/AppContext'
 
 const App = () => {
+
   const isAdminRoute = useLocation().pathname.startsWith('/admin')
+
+  const { user } = useAppContext()
 
   return (
     <>
@@ -30,7 +35,11 @@ const App = () => {
       <Route path='/movies/:id/:date' element={<SeatLayout/>} />
       <Route path='/my-bookings' element={<MyBookings/>} />
       <Route path='/favorite' element={<Favorite/>} />
-      <Route path='/admin/*' element={<Layout/>}>
+      <Route path='/admin/*' element={user ? <Layout/> : (
+        <div className='min-h-screen flex justify-center items-center'>
+          <SignIn fallbackRedirectUrl={'/admin'} />
+        </div>
+      )}>
         <Route index element={<Dashboard />}/>
         <Route path="add-shows" element={<AddShows />} />
         <Route path="list-shows" element={<ListShows />} />
