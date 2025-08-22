fbb2ccc5f526195caa413a4e073a80a4914439ec
pamudithasandaru <pamudithasandaru2002@gmail.com>
2025-08-22 16:27:33 +0530

    Active Shows grid in home page

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -2,15 +2,17 @@ import { ArrowRight } from 'lucide-react'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import BlurCircle from './BlurCircle'
-import { dummyShowsData } from '../assets/assets'
 import MovieCard from './MovieCard'
+import { useAppContext } from '../context/AppContext'
 
 const FeaturedSection = () => {
 
     const navigate = useNavigate()
+    const {shows } = useAppContext()
 
   return (
     <div className='px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden'>
+
         <div className='relative flex items-center justify-between pt-20 pb-10'>
             <BlurCircle top='0' right='-80px'/>
             <p className='text-gray-300 font-medium text-lg'>Now Showing</p>
@@ -21,7 +23,7 @@ const FeaturedSection = () => {
         </div>
 
         <div className='flex flex-wrap max-sm:justify-center gap-8 mt-8'>
-          {dummyShowsData.slice(0, 4).map((show)=>
+          {shows.slice(0, 4).map((show)=>
           (<MovieCard key={show._id} movie={show}/>
 
           ))}
