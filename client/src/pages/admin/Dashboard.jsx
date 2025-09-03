47c86f3258664bc7ed854f0525ecc1fe3f2ce063
pamudithasandaru <pamudithasandaru2002@gmail.com>
2025-09-04 02:05:46 +0530

    error correction

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -73,13 +73,17 @@ const Dashboard = () => {
         <BlurCircle top="100px" left="-10%" />
         {dashboardData.activeShows.map((show)=> (
             <div key={show._id} className="w-55 rounded-lg overflow-hidden h-full pb-3 bg-primary/10 border border-primary/20 hover:-translate-y-1 transition duration-300">
-                <img src={image_base_url + show.movie.poster_path} alt='' className="h-60 w-full object-cover" />
-                <p className="font-medium p-2 truncate">{show.movie.title}</p>
+                <img
+  src={show.movie?.poster_path ? image_base_url + show.movie.poster_path : "/fallback.jpg"}
+  alt={show.movie?.title || "No title"}
+  className="h-60 w-full object-cover"
+/>
+                <p className="font-medium p-2 truncate">{show?.movie?.title || "Untitled"}</p>
                 <div className="flex items-center justify-between px-2">
                     <p className="text-lg font-medium">{currency} {show.showPrice}</p>
                     <p className="flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1">
                         <StarIcon className="w-4 h-4 text-primary fill-primary" />
-                        {show.movie.vote_average.toFixed(1)}
+                        {(show.movie?.vote_average ?? 0).toFixed(1)}
                     </p>
                 </div>
                 <p className="px-2 pt-2 text-sm text-gray-500">{dateFormat(show.showDateTime)}</p>
